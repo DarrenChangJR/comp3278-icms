25bd537ef13ebbe7061d657785747ef97ea8fb9f
DarrenChangJR <darrenchangjr01@gmail.com>
2023-11-13 20:48:39 +0800

    Remove dummy.sql data

--- backend/dummy.sql
+++ backend/dummy.sql
@@ -1,12 +1,12 @@
-INSERT INTO student (name, email, last_login, last_logout) VALUES
-('John Doe', 'johndoe@example.com', '2023-01-02 12:00:00', '2023-01-02 12:10:00'),
-('Jane Nigga', 'janenigga@example.com', '2023-11-11 16:00:00', '2023-01-02 17:10:00');
+-- INSERT INTO student (name, email, last_login, last_logout) VALUES
+-- ('John Doe', 'johndoe@example.com', '2023-01-02 12:00:00', '2023-01-02 12:10:00'),
+-- ('Jane Nigga', 'janenigga@example.com', '2023-11-11 16:00:00', '2023-01-02 17:10:00');
 
-INSERT INTO course (code, semester, academic_year, name, moodle_link) VALUES
-('COMP3278', '2', '2023', 'Introduction to Database Management Nigga', 'https://nigga.com'),
-('FINA1310', '2', '2023', 'Introduction to Finance Nigger', 'https://nigger.com');
+-- INSERT INTO course (code, semester, academic_year, name, moodle_link) VALUES
+-- ('COMP3278', '2', '2023', 'Introduction to Database Management Nigga', 'https://nigga.com'),
+-- ('FINA1310', '2', '2023', 'Introduction to Finance Nigger', 'https://nigger.com');
 
-INSERT INTO takes (student_id, course_id) VALUES
-(1, 1),
-(1, 2),
-(2, 1);
+-- INSERT INTO takes (student_id, course_id) VALUES
+-- (1, 1),
+-- (1, 2),
+-- (2, 1);
